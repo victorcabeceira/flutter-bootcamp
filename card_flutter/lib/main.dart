13aca6e32f4732f1a865c96f1654faf44750bd89
victorcabeceira <victorfgcabeceira@gmail.com>
2020-02-16 23:19:29 -0300

    50: Using Cards amd ListTile instead of Rows

--- card_flutter/lib/main.dart
+++ card_flutter/lib/main.dart
@@ -13,70 +13,77 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
               radius: 50,
-              backgroundColor: Colors.white,
+              backgroundColor: Colors.blueGrey[100],
               backgroundImage: AssetImage('images/victor.jpg'),
             ),
             Text(
               'Victor Cabeceira',
               style: TextStyle(
-                  fontSize: 40, color: Colors.white, fontFamily: 'Sriracha'),
+                  fontSize: 40,
+                  color: Colors.blueGrey[100],
+                  fontFamily: 'Sriracha'),
             ),
             Text(
               'React, React Native and Flutter developer',
               style: TextStyle(
                 fontSize: 20,
-                color: Colors.white,
+                color: Colors.blueGrey[100],
                 fontFamily: 'Montserrat',
                 fontWeight: FontWeight.w400,
                 letterSpacing: 2.5,
               ),
+              textAlign: TextAlign.center,
             ),
-            Container(
-              color: Colors.white,
+            SizedBox(
+              height: 20,
+              width: 200,
+              child: Divider(
+                color: Colors.blueGrey[300],
+              ),
+            ),
+            Card(
               margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children: <Widget>[
-                  Icon(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: ListTile(
+                  leading: Icon(
                     Icons.phone,
-                    size: 20,
+                    size: 25,
                     color: Colors.blueGrey.shade700,
                   ),
-                  SizedBox(width: 10),
-                  Text(
+                  title: Text(
                     '+55 61 982 252 227',
                     style: TextStyle(
                         fontSize: 20,
                         color: Colors.blueGrey.shade700,
                         fontFamily: 'Montserrat'),
                   ),
-                ],
+                ),
               ),
             ),
-            Container(
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: Padding(
                 padding: EdgeInsets.all(10),
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.blueGrey.shade700,
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      'victorfgcabeceira@gmail.com',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.blueGrey.shade700,
-                          fontFamily: 'Montserrat'),
-                    ),
-                  ],
-                )),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 25,
+                    color: Colors.blueGrey.shade700,
+                  ),
+                  title: Text(
+                    'victorfgcabeceira@gmail.com',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.blueGrey.shade700,
+                        fontFamily: 'Montserrat'),
+                  ),
+                ),
+              ),
+            ),
           ],
         )),
       ),
