722d93a217cc8edc30dcf9e2fc2dea47a8356631
victorcabeceira <victorfgcabeceira@gmail.com>
2020-02-17 10:33:20 -0300

    55: Adding Expanded to fit dices in a row

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -18,6 +18,16 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        SizedBox(width: 10),
+        Expanded(
+          child: Image.asset('images/dice2.png'),
+        ),
+      ],
+    );
   }
 }
