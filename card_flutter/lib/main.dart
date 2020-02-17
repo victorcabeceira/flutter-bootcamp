d80a7c0ed28cb738c0713e330125b190b03eb4d9
victorcabeceira <victorfgcabeceira@gmail.com>
2020-02-16 22:25:23 -0300

    48: Adding custom fonts

--- card_flutter/lib/main.dart
+++ card_flutter/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.cyan[400],
+        backgroundColor: Colors.blueGrey[600],
         body: SafeArea(
             child: Column(
           children: <Widget>[
@@ -18,11 +18,23 @@ class MyApp extends StatelessWidget {
               backgroundColor: Colors.white,
               backgroundImage: AssetImage('images/victor.jpg'),
             ),
-            Text('Victor Cabeceira',
-                style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold))
+            Text(
+              'Victor Cabeceira',
+              style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'Montserrat'),
+            ),
+            Text(
+              'React, React Native and Flutter developer',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 2.5),
+            ),
           ],
         )),
       ),
