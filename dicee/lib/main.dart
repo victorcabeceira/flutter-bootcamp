31559e72eb7978e66083d33d0101241ae48a1d58
victorcabeceira <victorfgcabeceira@gmail.com>
2020-02-17 22:12:57 -0300

    56: Adding Padding

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -18,16 +18,24 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        SizedBox(width: 10),
-        Expanded(
-          child: Image.asset('images/dice2.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
