b709374fc47092e0e5dfa7343fbd8be969cc4914
victorcabeceira <victorfgcabeceira@gmail.com>
2020-02-18 11:22:37 -0300

    73: Step 3 of challenge

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -12,9 +12,22 @@ class BallPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.teal,
       appBar: AppBar(
-        backgroundColor: Colors.teal.shade800,
+        backgroundColor: Colors.teal.shade900,
         title: Text('Ask me anything!'),
       ),
+      body: Ball(),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Image.asset('images/ball1.png'));
   }
 }
