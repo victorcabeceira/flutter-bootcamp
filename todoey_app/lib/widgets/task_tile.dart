d2394f5992ec93b4865817b6d78439f212a2c70b
Victor <victorfgcabeceira@gmail.com>
2020-03-13 20:25:12 -0300

    213: Managing state from upper widget

--- todoey_app/lib/widgets/task_tile.dart
+++ todoey_app/lib/widgets/task_tile.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void toggleCheckbox(bool checkboxState) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Sample task'),
-      trailing: Checkbox(
-        value: false,
+      title: Text(
+        'Sample task',
+        style: isChecked
+            ? TextStyle(decoration: TextDecoration.lineThrough)
+            : null,
       ),
+      trailing: TaskCheckbox(
+          checkboxState: isChecked, toggleCheckboxState: toggleCheckbox),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
+
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxState,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: toggleCheckboxState,
     );
   }
 }
