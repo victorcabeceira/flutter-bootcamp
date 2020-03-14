23952f64252637598cd153168c3b82c3704aeb81
victorcabeceira <victorfgcabeceira@gmail.com>
2020-03-14 13:09:27 -0300

    215: Using ListView Builder

--- todoey_app/lib/widgets/tasks_list.dart
+++ todoey_app/lib/widgets/tasks_list.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
 import 'package:todoey_app/widgets/index.dart';
+import 'package:todoey_app/models/index.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'first'),
+    Task(name: 'second'),
+    Task(name: 'third'),
+  ];
+
+  void checkboxCallBack(bool checkboxState, int index) {
+    setState(() {
+      tasks[index].toggleDone();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+            taskTitle: tasks[index].name,
+            isChecked: tasks[index].isDone,
+            checkboxCallBack: (bool checkboxState) {
+              checkboxCallBack(checkboxState, index);
+            });
+      },
+      itemCount: tasks.length,
     );
   }
 }
