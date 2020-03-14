915d9e5882f553393bd13ca8ce3d6e289caa2688
victorcabeceira <victorfgcabeceira@gmail.com>
2020-03-14 20:34:29 -0300

    221: Updating todos to checked/unchecked

--- todoey_app/lib/widgets/tasks_list.dart
+++ todoey_app/lib/widgets/tasks_list.dart
@@ -5,22 +5,17 @@ import 'package:todoey_app/widgets/index.dart';
 import 'package:todoey_app/models/index.dart';
 
 class TasksList extends StatelessWidget {
-  void checkboxCallBack(bool checkboxState, int index) {
-    // setState(() {
-    //   widget.tasks[index].toggleDone();
-    // });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Consumer<TaskData>(builder: (context, taskData, child) {
       return ListView.builder(
         itemBuilder: (context, index) {
+          final task = taskData.tasks[index];
           return TaskTile(
-              taskTitle: taskData.tasks[index].name,
-              isChecked: taskData.tasks[index].isDone,
+              taskTitle: task.name,
+              isChecked: task.isDone,
               checkboxCallBack: (bool checkboxState) {
-                checkboxCallBack(checkboxState, index);
+                taskData.updateTask(task);
               });
         },
         itemCount: taskData.taskCount,
