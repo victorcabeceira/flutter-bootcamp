c6267d84ff9cd7cb6b3615e980c77b14198db44b
Victor <victorfgcabeceira@gmail.com>
2020-03-16 17:49:06 -0300

    Setting todoe_app provider to muiltiProvider

--- todoey_app/lib/main.dart
+++ todoey_app/lib/main.dart
@@ -11,8 +11,12 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => TaskData(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => TaskData(),
+        )
+      ],
       child: MaterialApp(
         home: TasksScreen(),
       ),
